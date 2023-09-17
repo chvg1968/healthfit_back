c3a9e2739d69850aef0515b83e89ae0eaecd0491
chvg1968 <conradovilla.gmail.com>
2023-09-17 16:59:56 -0500

    Update folders and files

--- src/REST-entities/session/session.model.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-
-const sessionSchema = new mongoose.Schema({
-  uid: mongoose.Types.ObjectId,
-});
-
-module.exports = mongoose.model("Session", sessionSchema);
--- src/helpers/function-helpers/try-catch-wrapper.js
+++
@@ -1,6 +0,0 @@
-module.exports = function (cb) {
-  return function (req, res, next) {
-    return cb(req, res, next).catch((err) => next(err));
-  };
-};
-
